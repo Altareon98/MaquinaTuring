718a4ffb209f21e243de6bdb16cf18fc32ba2f4d
its181 <soyjareth7u7@gmail.com>
2019-02-19 11:51:51 -0600

    Incorporació de Entrega

--- MT_Molde/MT_Copiadora/Form1.cs
+++ MT_Molde/MT_Copiadora/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Máquina de Turing
+//Por Lázaro y Jareth
+
 namespace MT_Copiadora
 {
 	public partial class Main : Form
